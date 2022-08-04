4864c609ecd706b7729a4d897956edc84524da32
jaey5oung <jaey5oung@naver.com>
2022-08-04 22:41:42 +0900

    ✨ feat : 라우팅

---
+++ src/pages/product/first-item.js
@@ -0,0 +1,18 @@
+import Layout from "../../components/Layout"
+import SubLayout from "../../components/SubLayout"
+
+export default function FirstItem({ time }) {
+  return (
+    <>
+      <h1 className="title">firstItem</h1>
+    </>
+  )
+}
+
+FirstItem.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <SubLayout>{page}</SubLayout>
+    </Layout>
+  )
+}
