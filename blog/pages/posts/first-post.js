d9e76554240741277fedfe7d7c0c83d4a4f24f32
jaey5oung <jaey5oung@naver.com>
2022-08-07 16:04:46 +0900

    ✨ feat : Image Component, Link Component 등 Next 제공하는 컴포넌트 활용해보기

---
+++ blog/pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Layout from "../../components/Layout"
+import Head from "next/head"
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>첫번째글</title>
+      </Head>
+      <h1>첫번째글</h1>
+    </Layout>
+  )
+}
